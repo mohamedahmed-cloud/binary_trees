292930971c4ccb68f8d73e0af8e9775bb73f7837
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-02 01:35:46 +0200

    Add File 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,5 +3,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *child = malloc(sizeof(binary_tree_t));
 
+	if (parent == NULL || child == NULL)
+		return (NULL);
+
+	child->n = value;
+	child->parent = parent;
+	child->left = parent->left;
+	child->right = NULL;
+
+	parent->left = child;
+
+	if (child->left)
+		child->left->parent = child;
+	return (child);
+
+	
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm a.out
-gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c main.c $1
+gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c main.c $1 $2
 ./a.out
