2427fabd60d4560f750184178dd362ab01d4d0fd
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-02 23:38:00 +0200

    Add File 10-binary_tree_depth.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Write a function that goes through a binary
+ * tree using pre-order traversal
+ * @tree: binary treee to make a proorder traversal
+ * @func: the pointer to print the value of node.
+*/
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+
+}
---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - Write a function that goes through a binary
+ * tree using in-order traversal
+ * @tree: binary treee to make a proorder traversal
+ * @func: the pointer to print the value of node.
+*/
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+
+}
