95571f5a5737ce196b1f621e086d07de9105be8e
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-02 21:44:05 +0200

    Add File 3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - function used to delete all tree.
+ * @tree: the tree deleted
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
