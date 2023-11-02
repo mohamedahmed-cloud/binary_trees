a2114f4f868f7541cddbf934f6fbb374e824125f
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-02 02:03:04 +0200

    Add File 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,12 +1,17 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_insert_right - function to insert in right
+ * @parent: the parent of inserted node
+ * @value: the value to insert in parent node.
+ * Return: null if failed esle return reference for inserted node.
+*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *child = malloc(sizeof(binary_tree_t));
 
 	if (!child || !parent)
-		return NULL;
-	
+		return (NULL);
+
 	child->n = value;
 	child->right = parent->right;
 	child->parent = parent;
@@ -15,8 +20,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (child->right)
 		child->right->parent = child;
-	
+
 	return (child);
-
-
 }
