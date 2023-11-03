f3568cf8b3809bf075a252e5f3751521be87ec75
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-03 13:39:14 +0200

    Add File 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *child = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL || child == NULL)
+	{
+		free(child);
 		return (NULL);
+	}
 
 	child->n = value;
 	child->parent = parent;
