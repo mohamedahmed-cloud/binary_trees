b3ca8f699554a6995e65b97437c3b7db05cd9527
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-11-03 14:02:37 +0200

    Add File 14-binary_tree_balance.c

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm a.out
-gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c main.c $1 $2 $3
+gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c main.c $1 $2 $3 $4
 ./a.out
